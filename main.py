94b0affc07b097ab0478a4801f64b05f5436998b
Tom van der Sande <thomasvandersande@kubrickgroup.com>
2025-11-05 13:48:32 +0000

    edit main

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 Script to run convert currencies based on command line inputs
 """
 from cli import cli_args
-from logger_config import logger
+import logger_config as log
 from fetcher import fetch_exchange_rate
 from converter import convert
 
@@ -12,15 +12,15 @@ def main():
     """
 
     args = cli_args()
-    logger.info(f"Arguments received: base={args.base}, target={args.target}, amount={args.amount}, mock={args.mock}")
+    # probs need log statement
 
     try:
         c_rate = fetch_exchange_rate(args.base, args.target)#, mock=args.mock) #Conversion Rate
         output = convert(args.amount, c_rate) #Output after conversion
-        logger.info(f"Conversion result: {args.amount} {args.base} = {output:.4f} {args.target}")
+        # probs need log statement
         print(f"Conversion: {args.amount} {args.base} = {output:.4f} {args.target}")
     except Exception as e:
-        logger.critical(f"Critical error during conversion: {e}")
+        # probs need log error statement
         print(f"Error: {e}")
 
 if __name__ == "__main__":
