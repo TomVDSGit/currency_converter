0569495089434ef84dff8f4bd4561a0b5a2cba71
Tom van der Sande <thomasvandersande@kubrickgroup.com>
2025-11-05 15:53:07 +0000

    Checked pytest and ran black

--- src/cli.py
+++ src/cli.py
@@ -1,7 +1,9 @@
 """
 Script to setup the CLI arguments
 """
-import argparse    
+
+import argparse
+
 
 def cli_args():
     """
@@ -12,14 +14,36 @@ def cli_args():
     parser = argparse.ArgumentParser(description="Currency conversion.")
 
     # Add arguments
-    parser.add_argument('--base', '-b', nargs='?', default='USD', type=str,
-                        help="Enter the currency you want to start with e.g USD")
-    parser.add_argument('--target', '-t', nargs='?', default='EUR', type=str,
-                        help="Enter the currency you want to convert to e.g EUR")
-    parser.add_argument('--amount', '-a', nargs='?', default=100, type=float,
-                        help="Enter the amount of base currency you want to convert e.g 100")
-    parser.add_argument('--mock', '-m', action='store_true',
-                        help="Use offline data instead of accessing the API")
+    parser.add_argument(
+        "--base",
+        "-b",
+        nargs="?",
+        default="USD",
+        type=str,
+        help="Enter the currency you want to start with e.g USD",
+    )
+    parser.add_argument(
+        "--target",
+        "-t",
+        nargs="?",
+        default="EUR",
+        type=str,
+        help="Enter the currency you want to convert to e.g EUR",
+    )
+    parser.add_argument(
+        "--amount",
+        "-a",
+        nargs="?",
+        default=100,
+        type=float,
+        help="Enter the amount of base currency you want to convert e.g 100",
+    )
+    parser.add_argument(
+        "--mock",
+        "-m",
+        action="store_true",
+        help="Use offline data instead of accessing the API",
+    )
 
     # Parse arguments to output
     return parser.parse_args()
--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
 """
 Script to run convert currencies based on command line inputs
 """
+
 from cli import cli_args
 import logger_config as log
 from fetcher import fetch_exchange_rate
 from converter import convert
+
 
 def main():
     """
@@ -12,17 +14,24 @@ def main():
     """
 
     args = cli_args()
-    log.logger.info(f"Starting conversion: {args.amount} {args.base} to {args.target} (mock={args.mock})")
+    log.logger.info(
+        f"Starting conversion: {args.amount} {args.base} to {args.target} (mock={args.mock})"
+    )
 
     try:
-        c_rate = fetch_exchange_rate(args.base, args.target, args.mock) #Conversion Rate
-        output = convert(args.amount, c_rate) #Output after conversion
-        log.logger.info(f"Conversion successful: {args.amount} {args.base} = {output:.4f} {args.target}")
+        c_rate = fetch_exchange_rate(
+            args.base, args.target, args.mock
+        )  # Conversion Rate
+        output = convert(args.amount, c_rate)  # Output after conversion
+        log.logger.info(
+            f"Conversion successful: {args.amount} {args.base} = {output:.4f} {args.target}"
+        )
         print(f"Conversion: {args.amount} {args.base} = {output:.4f} {args.target}")
     except Exception as e:
         log.logger.error(f"Error occurred: {e}")
         print(f"Error: {e}")
         log.logger.info("Currency converter application terminated with errors")
+
 
 if __name__ == "__main__":
     main()
