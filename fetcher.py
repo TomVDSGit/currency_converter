829d9ce02bec5a32e80f98bcd2ba4eb625974391
Taisy Scott <taisyscott@kubrickgroup.com>
2025-11-05 12:45:22 +0000

    mock file input fixed

--- fetcher.py
+++ fetcher.py
@@ -4,42 +4,40 @@ A fetcher script that fetches exchange rates from an external API.
 
 import os
 import sys
-import argparse
 import json
 import requests
 from dotenv import load_dotenv
 from logger_config import logger
 
 
-def fetch_exchange_rate(base: str, target: str) -> float:
+def fetch_exchange_rate(base: str, target: str, mock_file: str = None) -> float:
     """
     Fetch exchange rate from base to target currency.
     """
-
-    parser = argparse.ArgumentParser(description="Input file for conversions.")
-    parser.add_argument('--file', '-f', nargs='?',
-                        help="Enter a JSON file with mock rates")
-    args = parser.parse_args()
-
-    if args.file: # If a mock file is provided, use it instead of the API
-        with open(args.file, 'r') as file:
-            data = json.load(file)
+    
+    if mock_file:
+        with open(mock_file, 'r') as file:
+            response_data = json.load(file)
     else:
         load_dotenv()
-        api_key = os.getenv("API_KEY")  # Accessing API key from .env file
-        data = f"http://api.exchangeratesapi.io/v1/latest?access_key={api_key}&symbols={base},{target}"
+        api_key = os.getenv("API_KEY")
+        url = f"http://api.exchangeratesapi.io/v1/latest?access_key={api_key}&symbols={base},{target}"
+
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
+            response_data = response.json()
+        except requests.RequestException as e:
+            print(f"Error fetching exchange rate: {e}")
+            logger.critical(f"Error fetching exchange rate: {e}")
+            sys.exit(1)
 
     try:
-        response = requests.get(data)
-        response.raise_for_status()
-        data = response.json()
-        base_rate = data["rates"].get(base)
-        target_rate = data["rates"].get(target)
-        logger.info(
-            f"Fetched exchange rates: {base}={base_rate}, {target}={target_rate}"
-        )
-        return target_rate / base_rate  # Returns final converstion rate
-
+        base_rate = response_data["rates"].get(base)
+        target_rate = response_data["rates"].get(target)
+        logger.info(f"Fetched exchange rates: {base}={base_rate}, {target}={target_rate}")
+        return target_rate / base_rate
+    
     except requests.RequestException as e:
         print(f"Error fetching exchange rate: {e}")
         logger.critical(f"Error fetching exchange rate: {e}")
