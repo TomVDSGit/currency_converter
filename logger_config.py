8e313cb84b3dba0901a72302bdf328f3cc0b38f9
Chinazom Okafor <chinazomokafor@kubrickgroup.com>
2025-11-05 11:47:40 +0000

    quick change

--- logger_config.py
+++ logger_config.py
@@ -3,7 +3,7 @@ import logging
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
-custom_file_handler = logging.FileHandler('todo_logging.log')
+custom_file_handler = logging.FileHandler('currency_converter.log')
 custom_file_handler.setLevel(logging.INFO)
 
 custom_file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(lineno)d - %(message)s')
