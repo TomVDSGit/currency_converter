3444c2ee8757f4b472c92700bcc8ccc39b26ba02
Taisy Scott <taisyscott@kubrickgroup.com>
2025-11-05 12:32:59 +0000

    fetcher adding JSON input

--- converter.py
+++ converter.py
@@ -2,11 +2,9 @@
 A script that converts currency amounts using fetched exchange rates from fetcher.py.
 '''
 
-import logging
 from logger_config import logger
 from typing import Dict
 
-logger = logging.getLogger(__name__)
 
 class CurrencyConverter:
     '''
@@ -14,11 +12,11 @@ class CurrencyConverter:
     '''
     def convert(self, amount: float, exchange_rate: float) -> float:
         if amount < 0:
-            log.logger.error("Attempted to convert a negative amount")
+            logger.error("Attempted to convert a negative amount")
             raise ValueError("Amount cannot be negative")
         
         result = amount * exchange_rate
-        log.logger.info(f"Converted {amount} at rate {exchange_rate} = {result:.2f}")
+        logger.info(f"Converted {amount} at rate {exchange_rate} = {result:.2f}")
         
         return round(result, 2)
 
--- fetcher.py
+++ fetcher.py
@@ -1,33 +1,51 @@
-'''
+"""
 A fetcher script that fetches exchange rates from an external API.
-'''
+"""
 
 import os
 import sys
+import argparse
+import json
 import requests
 from dotenv import load_dotenv
 from logger_config import logger
 
+
 def fetch_exchange_rate(base: str, target: str) -> float:
-    '''
+    """
     Fetch exchange rate from base to target currency.
-    '''
-    load_dotenv()
-    api_key = os.getenv("API_KEY") # Accessing API key from .env file
-    url = f"http://api.exchangeratesapi.io/v1/latest?access_key={api_key}&symbols={base},{target}"
+    """
+
+    parser = argparse.ArgumentParser(description="Input file for conversions.")
+    parser.add_argument('--file', '-f', nargs='?',
+                        help="Enter a JSON file with mock rates")
+    args = parser.parse_args()
+
+    if args.file: # If a mock file is provided, use it instead of the API
+        with open(args.file, 'r') as file:
+            data = json.load(file)
+    else:
+        load_dotenv()
+        api_key = os.getenv("API_KEY")  # Accessing API key from .env file
+        data = f"http://api.exchangeratesapi.io/v1/latest?access_key={api_key}&symbols={base},{target}"
+
     try:
-        response = requests.get(url)
+        response = requests.get(data)
         response.raise_for_status()
         data = response.json()
         base_rate = data["rates"].get(base)
         target_rate = data["rates"].get(target)
-        log.logger.info(f"Fetched exchange rates: {base}={base_rate}, {target}={target_rate}")
-        return target_rate / base_rate # Returns final converstion rate
+        logger.info(
+            f"Fetched exchange rates: {base}={base_rate}, {target}={target_rate}"
+        )
+        return target_rate / base_rate  # Returns final converstion rate
+
     except requests.RequestException as e:
         print(f"Error fetching exchange rate: {e}")
-        log.logger.critical(f"Error fetching exchange rate: {e}")
+        logger.critical(f"Error fetching exchange rate: {e}")
         sys.exit(1)
+
     except KeyError:
         print(f"Invalid target currency '{target}' or no rate available.")
-        log.logger.critical(f"Error fetching exchange rate: {e}")
+        logger.critical(f"Error fetching exchange rate: {e}")
         sys.exit(1)
