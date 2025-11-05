33332d4b8bd3cd9e05a01cafec0ee145a5dadd81
Chinazom Okafor <chinazomokafor@kubrickgroup.com>
2025-11-05 15:44:50 +0000

    refactering test code

--- tests/test_converter.py
+++ tests/test_converter.py
@@ -1,10 +1,4 @@
-import sys
-import os
-import pytest
 from src import main
-
-#     (["--base", "USD", "--target", "EUR", "--amount", "100"]),
-#     (["--base", "GBP", "--target", "JPY", "--amount", "200"]),
 
 
 def test_currency_converter(monkeypatch, capfd):
